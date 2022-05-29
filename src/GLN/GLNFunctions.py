d01ae1be4b89ce28b197d797f5e7ddba472d9b0a
Luis Girón Juárez <79474023+lgj19@users.noreply.github.com>
2022-05-29 03:18:39 +0200

    Actualización de la función de sinónimos

--- src/GLN/GLNFunctions.py
+++ src/GLN/GLNFunctions.py
@@ -1,17 +1,14 @@
-from googletrans import Translator
 from nltk.corpus import wordnet
 
 def obtenerSinonimos(palabra):
-    translator = Translator()
-    word = translator.translate(palabra, src='es')
-
     sinonimos = []
-    for syn in wordnet.synsets(word.text):
+
+    for syn in wordnet.synsets(palabra, lang='spa'):
         for name in syn.lemma_names('spa'):
-            sinonimos.append(name)
+            if(name != palabra):
+                sinonimos.append(name)
 
     return set(sinonimos)
 
 
 
-    
